e6c5b5247c07238227a66625446a4119e6e7102e
Dante <drdrum16@gmail.com>
2026-09-19 00:42:19 -0400

    Trying to fix app

--- backend.py
+++ backend.py
@@ -6,8 +6,6 @@ import streamlit as st
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-if "OPENAI_API_KEY" in st.secrets:
-    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 def get_llm(model_choice):
     if model_choice == "GPT-5.4 regular (advanced reasoning)":
