7a89047cdb7d6cdf0ca43473962f4f9418a32355
Dante <drdrum16@gmail.com>
2026-09-19 00:38:44 -0400

    Trying to fix app

--- backend.py
+++ backend.py
@@ -4,7 +4,10 @@ from langchain_openai import ChatOpenAI
 import streamlit as st
 
 load_dotenv()
-OPENAI_API_KEY = st.secrets.get("OPENAI_API_KEY") or os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+if "OPENAI_API_KEY" in st.secrets:
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 
 def get_llm(model_choice):
     if model_choice == "GPT-5.4 regular (advanced reasoning)":
